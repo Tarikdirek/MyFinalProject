af0f82a21c8cf243393840153af5783c3b6be34b
Tarık Direk <tarkdirek@gmail.com>
2023-02-02 17:11:32 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProductManager productManager = new ProductManager(new InMemoryProductDal());
+        foreach (var product in productManager.GetAll())
+        {
+            Console.WriteLine(product.ProductName);
+        }
+        
+    }
+}
